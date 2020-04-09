2ea815b508f25605d55e7c879273be05f8caae02
Manjot G <kanwar.johal@reframeinc.com>
2020-04-09 12:50:25 -0400

    update

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ def handle_framing_request():
             req = request.get_json()
             print('sent from client', req)
         else:
-            print('sent form client', request.args, request.values, request.form, request.form)
+            print('sent form client', request.get('occupancy'))
     return 'hollowcore'
 
 
