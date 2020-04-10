30b071b970efe019b8e790335eae16806f4b1034
Manjot G <kanwar.johal@reframeinc.com>
2020-04-09 21:39:53 -0400

    update

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import os
 import psycopg2
+from flask import jsonify
 
 from stubbeshollowcore import *
 
@@ -120,10 +121,11 @@ def handle_framing_request():
         if request.is_json:
             req = request.get_json()
             # req = {'userinput': ['Assembly areas', 'Platforms', 100], 'span': '61'}
-            design_dict = designstubbeshc(fl_uniform_live_load=req.get('userinput')[2], fl_span=req.get('span'),
-                            str_units='imperial',
-                            db_hollowcore=StubbesHollowCore)
-            return design_dict
+            design_dict = designstubbeshc(fl_uniform_live_load=float(req.get('userinput')[2]),
+                                          fl_span=float(req.get('span')),
+                                          str_units='imperial',
+                                          db_hollowcore=StubbesHollowCore)
+            return jsonify(design_dict)
         else:
             return {"error": "The request payload is not in JSON format"}
 
--- stubbeshollowcore.py
+++ stubbeshollowcore.py
@@ -11,13 +11,13 @@ def designstubbeshc(fl_uniform_live_load, fl_span, str_units, db_hollowcore):
         """
         fl_uniform_live_load = fl_uniform_live_load/20.9
         fl_span = fl_span*0.3048
-        
+
         fl_mf_live = moment_simple_beam(fl_span=fl_span, fl_width=1, fl_loading=fl_uniform_live_load)
 
         for hollow_core in database_hc:
-            fl_mf_dead = moment_simple_beam(fl_span=fl_span, fl_width=1, fl_loading=hollow_core.swkpa)
+            fl_mf_dead = moment_simple_beam(fl_span=fl_span, fl_width=1, fl_loading=float(hollow_core.swkpa))
             fl_mu = 1.25 * fl_mf_dead + 1.5 * fl_mf_live
-            fl_mr = hollow_core.mrnmm/1000000
+            fl_mr = float(hollow_core.mrnmm)/1000000
             fl_utilization = fl_mu/fl_mr
 
             design_dict.update({"depth": hollow_core.depthmm,
